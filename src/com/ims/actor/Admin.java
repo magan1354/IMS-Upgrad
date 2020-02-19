151aaabd503c7c211c4c7bdafd1f9f1eb692bd4f
Maganjot Singh <Maganjot.Singh@upgrad.local>
2020-02-19 19:29:18 +0530

    Created Getters and Setters for Admin

--- src/com/ims/actor/Admin.java
+++ src/com/ims/actor/Admin.java
@@ -3,16 +3,16 @@ package com.ims.actor;
 class Admin {
     //DataType nameofattribute
     int id;
-    String name;
-    Address address;
-    Supplier[] suppliers = new Supplier[1000];
+    private String name;
+    private Address address;
+    private Supplier[] suppliers = new Supplier[1000];
 
     Admin(){
         System.out.println("Admin Constructed !!");
     }
 
     Admin(String n, Address address){
-        name = n;
+        setName(n);
         this.address = address;
     }
 
@@ -42,7 +42,30 @@ class Admin {
 
     }
 
-    String getName(){
+    public String getName(){
         return name;
+    }
+
+    public void setName(String name){
+        if(name == null || name == ""){
+            // Generate an alarm
+        }
+        this.name = name;
+    }
+
+    public int getId(){
+        return id;
+    }
+
+    public void setId(int id){
+        this.id = id;
+    }
+
+    public void setAddress(Address address){
+        this.address = address;
+    }
+
+    public Address getAddress(){
+        return address;
     }
 }
